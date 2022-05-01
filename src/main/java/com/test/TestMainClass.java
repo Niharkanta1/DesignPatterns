74eaa925246df662b7ed2a393d33222c74f1f3ea
Nihar-Desktop <niharkanta1@live.com>
2022-05-01 23:37:31 +0530

    First Commit + gitignore

---
+++ src/main/java/com/test/TestMainClass.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/*
+ * @created 01-05-2022
+ * @project DesignPatterns
+ * @author Nihar
+ */
+public class TestMainClass {
+	public static void main(String[] args) {
+		System.out.println("Test....");
+	}
+}
